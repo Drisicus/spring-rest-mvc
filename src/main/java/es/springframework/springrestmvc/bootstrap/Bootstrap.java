3c028b69ad2994c69a83e71a34099e283fed67fa
Drisicus <aaasssccc@gmail.com>
2020-11-20 19:41:46 +0100

    Add Bootstrap class to add initial Categories using CommandLineRunner. Fix Category to be jpa entity

---
+++ src/main/java/es/springframework/springrestmvc/bootstrap/Bootstrap.java
@@ -0,0 +1,44 @@
+package es.springframework.springrestmvc.bootstrap;
+
+import es.springframework.springrestmvc.domain.Category;
+import es.springframework.springrestmvc.repositories.CategoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class Bootstrap implements CommandLineRunner {
+    private CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        log.info("Data Loaded: {}", categoryRepository.count());
+    }
+}
--- src/main/java/es/springframework/springrestmvc/domain/Category.java
+++ src/main/java/es/springframework/springrestmvc/domain/Category.java
@@ -1,12 +1,14 @@
 package es.springframework.springrestmvc.domain;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Data
+@Entity
 public class Category {
 
     @Id
