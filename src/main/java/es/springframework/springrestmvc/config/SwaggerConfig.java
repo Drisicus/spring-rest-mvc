f3b6c5cd4dace0b5cecc62e292aa1b7e48df733d
Drisicus <aaasssccc@gmail.com>
2020-11-29 13:36:20 +0100

    Add custom ApiInfo for swagger

--- src/main/java/es/springframework/springrestmvc/config/SwaggerConfig.java
+++ src/main/java/es/springframework/springrestmvc/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -18,7 +22,20 @@ public class SwaggerConfig {
                 .select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
+    }
+
+
+    private ApiInfo metaData(){
+        Contact contact = new Contact("Andrés", "https://github.com/Drisicus/", "");
+        return new ApiInfo("Spring Rest MVC",
+                "Example proyect for Spring Rest MVC",
+                "1.0",
+                "Terms of service: todo",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
     }
 
 }
